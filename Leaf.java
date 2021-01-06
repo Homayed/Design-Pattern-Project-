89216bbcb6cead578b22657fe556a7487b6353c2
Homayed <39415294+Homayed@users.noreply.github.com>
2021-01-06 18:53:18 +0600

    Add files via upload

---
+++ Leaf.java
@@ -0,0 +1,13 @@
+public class Leaf implements Shape {
+    String name;
+
+    public Leaf(String name) {
+        super();
+        this.name = name;
+    }
+
+    public void showShapeType() {
+        System.out.println(name);
+    }
+
+}
